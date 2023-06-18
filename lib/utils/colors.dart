d3503d4add3f625c690fc241923078b4734d1b58
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-19 00:01:31 +0600

    colors added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xff1f212c);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color sliderColor = Color(0xff7e70ff);
+  static const Color buttonColor = Color(0xff60e95d);
+  static const Color bgDarkColor = Color(0xff070b11);
+}
