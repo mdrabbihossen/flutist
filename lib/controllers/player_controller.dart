b596261d9ac00333643ac6c99bdc3568b79d17ba
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-19 18:23:45 +0600

    player controller updated with permission

--- lib/controllers/player_controller.dart
+++ lib/controllers/player_controller.dart
@@ -1,5 +1,30 @@
 import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class PlayerController extends GetxController{
+class PlayerController extends GetxController {
+  final audioQuery = OnAudioQuery();
 
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    checkPermission();
+  }
+
+  // check permission
+  checkPermission() async {
+    var permission = await Permission.storage.request();
+    if (permission.isGranted) {
+      return audioQuery.querySongs(
+        ignoreCase: true,
+        orderType: OrderType.ASC_OR_SMALLER,
+        sortType: null,
+        uriType: UriType.EXTERNAL,
+      );
+    } else{
+      checkPermission();
+    }
+  }
+  // check permission end
 }
